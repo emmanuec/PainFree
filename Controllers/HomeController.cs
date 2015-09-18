cccb6a16cd562cb037d65c2d4df67b43af53542d
emmanuec <emmanuec@gmail.com>
2015-09-17 23:35:57 -0700

    Controller files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AutomationManagementTool.Models;
+using System.Web.Mvc;
+
+namespace AutomationManagementTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TestReports testReports = TestReports.getInstance();
+
+        public ActionResult Index()
+        {
+            var failedTestReports = testReports.getFailedTestReports();
+            var test = TestReports.getNoonRunTestReports(failedTestReports);
+
+            return View(failedTestReports);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
